aa19b4aa3cbd50b047583d0f8bdea0f54daae3ac
yun8565 <yun8565@knu.ac.kr>
2024-09-10 10:32:13 +0900

    initial commit

---
+++ src/main/java/com/dxschool/lightme/LightmeApplication.java
@@ -0,0 +1,13 @@
+package com.dxschool.lightme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LightmeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LightmeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dxschool/lightme/LightmeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dxschool.lightme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LightmeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
